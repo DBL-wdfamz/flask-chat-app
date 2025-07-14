2f82cfd7c3c2c6bd1d679237a1f09a4bb8508451
DBL <78522805+DBL-wdfamz@users.noreply.github.com>
2025-07-14 18:13:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -379,4 +379,4 @@ if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     init_db()
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
